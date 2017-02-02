0431846a66ac1797b9ae64d647637b57376358af
Richard Li <aspirewit@gmail.com>
2017-02-02 21:55:45 +0800

    Export workflow file

--- workflow/commands.sh
+++ workflow/commands.sh
@@ -1,20 +1,18 @@
 mac_commands=(
-    apps:close-all
+    close-all
     eject-all
 )
+
+if [ $1 ]; then
+  mac_commands=( $( for command in ${mac_commands[@]} ; do echo $command ; done | grep $1 ) )
+fi
 
 cat<<EOB
 <?xml version="1.0"?>
 <items>
 EOB
 
-if [ -z $1 ]; then
-  mac_commands=$mac_commands
-else
-  matched_commands=( $( for command in ${mac_commands[@]} ; do echo $command ; done | grep $1 ) )
-fi
-
-for command in "${matched_commands[@]}"; do
+for command in "${mac_commands[@]}"; do
 cat<<EOB
     <item arg="$command">
         <title>$command</title>
